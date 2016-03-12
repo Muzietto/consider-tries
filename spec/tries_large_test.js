17e2074bc6be267506f3491b1559973e552e8579
Muzietto <marco_faustinelli@yahoo.com>
2016-03-12 11:06:57 +0100

    starting to write json files for real

--- spec/tries_large_test.js
+++ spec/tries_large_test.js
@@ -16,14 +16,15 @@
   var Q = require('q');
 
   var H = require(__dirname + '/../util/helper');
-  var TRIE = require('../js/tries').TRIE;
+  var TRIE = require(__dirname + '/../js/tries').TRIE;
   
   var fs = require('fs');
+  var jsonfile = require('jsonfile');
   var path = require('path');
   
   describe('after loading arrays of words from large files', function() {
     beforeEach(function() {
-      this.words = readAndSplit('../doc/considerPhlebas.txt');
+      this.words = readAndSplit('/../doc/considerPhlebas.txt');
     });
     it('can build very large tries', function() {
       this.words.then(function(data) {
@@ -31,38 +32,40 @@
         H.log('Start building the trie: ' + new Date().toString());
         var trie = {},counter=0;
         data.forEach(function(word) {
-          H.log(++counter+': ' + word.toLowerCase() + '                                                  \033[F');
+          H.log(++counter + ': ' + word.toLowerCase() + '                                                  \033[F');
           trie = TRIE.put(word.toLowerCase(), TRIE.incr, trie);
         });
         H.log(counter);
         var endMillis = new Date().getTime();
         var elapsed;
-        try { elapsed = (endMillis - startMillis)/1000} catch (e) {}
+        try { elapsed = (endMillis - startMillis)/1000 } catch (e) {}
         H.log('Trie complete: ' + new Date().toString());
         H.log('Processing time in secs: ' + elapsed);
         H.log('TRIE.size(trie): ' + TRIE.size(trie));
       });
     });
-    it.only('can sort large lists and build a trie with the ordered words', function() {
+    it('can sort large lists and build a trie with the ordered words', function() {
       this.words.then(function(data) {
+        var startMillis = new Date().getTime();
         H.log('Start sorting the list: ' + new Date().toString());
         var trie = {}, sorted = data.sort(),counter=0;
         H.log('Start building the trie: ' + new Date().toString());
         sorted.forEach(function(word) {
-          H.log(++counter+': ' + word.toLowerCase() + '                                                  \033[F');
+          H.log(++counter + ': ' + word.toLowerCase() + '                                                  \033[F');
           trie = TRIE.put(word.toLowerCase(), TRIE.incr, trie);
         });
         H.log(counter);
         var endMillis = new Date().getTime();
         var elapsed;
-        try { elapsed = (endMillis - startMillis)/1000} catch (e) {}
+        try { elapsed = (endMillis - startMillis)/1000 } catch (e) {}
         H.log('Trie complete: ' + new Date().toString());
         H.log('Processing time in secs: ' + elapsed);
         H.log('TRIE.size(trie): ' + TRIE.size(trie));
       });
     });
-    it.only('can make count tables out of large sorted lists and build a trie with the counts', function() {
+    it('can make count tables out of large sorted lists and build a trie with the counts', function() {
       this.words.then(function(data) {
+        var startMillis = new Date().getTime();
         H.log('Start sorting the list: ' + new Date().toString());
         var trie = {}, sorted = data.sort(), counter = 0, counts = {};
         H.log('Start building the count table: ' + new Date().toString());
@@ -88,10 +91,58 @@
         H.log(counter);
         var endMillis = new Date().getTime();
         var elapsed;
-        try { elapsed = (endMillis - startMillis)/1000} catch (e) {}
+        try { elapsed = (endMillis - startMillis)/1000 } catch (e) {}
         H.log('Trie complete: ' + new Date().toString());
         H.log('Processing time in secs: ' + elapsed);
         H.log('TRIE.size(trie): ' + TRIE.size(trie));
+      });
+    });
+  });
+  describe('willing to prepare half-baked results', function() {
+    beforeEach(function() {
+      this.filename = '/../doc/considerPhlebas.txt';
+      this.words = readAndSplit(this.filename);
+    });
+    it.only('one can save large tries as JSON to use them later', function() {
+      var self = this;
+      return this.words.then(function(data) {
+        var startMillis = new Date().getTime();
+        H.log('Start sorting the list: ' + new Date().toString());
+        var trie = {}, sorted = data.sort(), counter = 0, counts = {};
+        H.log('Start building the count table: ' + new Date().toString());
+        sorted.forEach(function(word) {
+          var _word = word.toLowerCase();
+          H.log(++counter + ': ' + _word + '                                                  \033[F');
+          if (!counts[_word]) {
+            counts[_word] = 1;
+            return;
+          }
+          counts[_word]++;
+          return;
+        });
+        counter = 0; 
+        H.log('End building the count table: ' + new Date().toString());
+        H.log('Count table keys: ' + Object.keys(counts).length);
+        H.log('Start building the trie: ' + new Date().toString());
+        Object.keys(counts).forEach(function(_word) {
+          var count = counts[_word];
+          H.log(++counter + ': ' + _word + ' -> ' + count + '                                                     \033[F');
+          trie = TRIE.put(_word, count, trie);
+        });
+        H.log(counter);
+        var endMillis = new Date().getTime();
+        var elapsed;
+        try { elapsed = (endMillis - startMillis)/1000 } catch (e) {}
+        H.log('Trie complete: ' + new Date().toString());
+        H.log('Processing time in secs: ' + elapsed);
+        H.log('TRIE.size(trie): ' + TRIE.size(trie));
+        var fileversion = Math.floor(Math.random() * 100000);
+        var jsonfilename = __dirname + self.filename + '.' + fileversion + '.json'
+        H.log('About to save trie in : ' + jsonfilename);
+        jsonfile.writeFile(jsonfilename, trie, { spaces: 2 }, function(err) {
+          console.error(err);
+        })
+        H.log('Trie saved in : ' + jsonfilename);
       });
     });
   });
